1f27a5988859adbb74015b71e6dcf1da7a14140e
tsogp <68006452+tsogp@users.noreply.github.com>
2021-02-10 20:13:29 +0300

    third commit
    making it possible to host on heroku

--- WeightApp/urls.py
+++ WeightApp/urls.py
@@ -1,8 +1,19 @@
-
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Register_page.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
+
+urlpatterns = urlpatterns + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
